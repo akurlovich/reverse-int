7715a2fdee36207dfa687a9c706d44898c5ff40d
Artsiom Kurlovich <qa.a.kurlovich@gmail.com>
2021-03-01 13:10:35 +0300

    add simple function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0) {
+    n = n * (-1);
+  } else {
+    n.toString().split().reverse()
+  }
+}
+
+
+let n = 432;
+if (n < 0) {
+  n = n * (-1);
+} else {
+  console.log(n.toString().split());
 }
